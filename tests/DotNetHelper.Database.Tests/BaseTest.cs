4713aa12296282e08a5a51c21fb3663c4398ef4b
Joseph McNeal Jr <josephmcnealjr@mofade.com>
2019-08-29 19:42:50 -0500

    added more unit test and updated nuget package

--- tests/DotNetHelper.Database.Tests/BaseTest.cs
+++ tests/DotNetHelper.Database.Tests/BaseTest.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
-using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Xml.Serialization;
 using CsvHelper.Configuration;
 using DotNetHelper.Database.Interface;
 using DotNetHelper.Database.Tests.Helpers;
